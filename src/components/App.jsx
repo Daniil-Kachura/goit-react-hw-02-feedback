5a3624895725c1c96794a26fdf844a7092ea89ae
Daniil-Kachura <ka4urad@gmail.com>
2022-10-11 21:05:39 +0300

    feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,73 @@
-import { Section } from "./Section/Section";
+import React, { Component } from 'react';
+import Section from './Section/Section';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+import Notification from './Notification/Notification';
 
-export const App = () => {
-  return (
-    < >
-    <div
-      style={{
-        backgroundImage: `linear-gradient(to right, #EAECC6, #2BC0E4)`,
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      <Section title={'Please leave feedback'} />
-     
-    </div> </>
-  );
-};
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = state => {
+    this.setState(prevState => ({
+      [state]: prevState[state] + 1,
+    }));
+  };
+
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+
+  countPositiveFeedbackPercentage() {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  }
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const options = Object.keys(this.state);
+    const totalFeedback = this.countTotalFeedback();
+
+    return (
+      <article className='wrapper'>
+      <div
+        style={{
+          display: 'inline-flex',
+          flexDirection: 'column',
+          textAlign: 'center',
+          margin: '20px',
+          paddingLeft: '10px',
+          paddingRight: '10px',
+          color: '#010101',
+          border: '2px solid black',
+          borderRadius: '6px',
+          backgroundImage: `linear-gradient(to right, #EAECC6, #2BC0E4)`
+        }}
+      >
+        <Section title="Please leave your feedback">
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          {totalFeedback > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={totalFeedback}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
+      </div></article>
+    );
+  }
+}
+export default App;
